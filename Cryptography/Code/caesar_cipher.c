99a25899a4bf0dc30dc5ee0b9418f816a4209e0f
Drashti Gadhvi <gadhvidrashti007@gmail.com>
2025-10-28 20:23:23 +0530

    Implement main function for Caesar cipher
    Added main function to handle user input for encryption and decryption.

--- Cryptography/Code/caesar_cipher.c
+++ Cryptography/Code/caesar_cipher.c
@@ -5,3 +5,11 @@
 // Function prototypes
 void encrypt (char text[], int key);
 void decrypt (char text[], int key);
+
+int main() {
+  char text[500];
+  int key, choice;
+
+  printf("===Caesar Cipher Implementation===\n");
+  printf("1. Encrypt \n2. Decrypt \nChoose an option (1/2):");
+  
